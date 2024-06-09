41c4536cb60e1bf19fab3951eb27b05514b10500
“kebean” <chenqiua@gmail.com>
2024-06-09 16:49:03 +0200

    finish and test functionality

--- challenges/arrayMap.js
+++ challenges/arrayMap.js
@@ -1,28 +1,21 @@
-// function hasContiguousSubarrayWithSum(arr, target) {
-//     let Sum = 0;
-//     let startIndex = 0;
-
-//     for (let i = 0; i < arr.length; i++) {
-//         Sum += arr[i];
-        
-//         while (Sum > target && startIndex <= i) {
-//             Sum -= arr[startIndex];
-//             startIndex++;
-//         }
-        
-//         if (Sum === target) {
-//             return true;
-//         }
-//     }
-
-//     return false;
-// }
-
-function hasContiguousSubarrayWithSum(arr, target){
+function hasContiguousSubarrayWithSum(arr, target) {
     let Sum = 0;
     let startIndex = 0;
-    for (let i = 0; i < arr.length; i++){
+
+    for (let i = 0; i < arr.length; i++) {
         Sum += arr[i];
         
+        while (Sum > target && startIndex <= i) {
+            Sum -= arr[startIndex];
+            startIndex++;
+        }
+        
+        if (Sum === target) {
+            return true;
+        }
     }
+
+    return false;
 }
+
+
