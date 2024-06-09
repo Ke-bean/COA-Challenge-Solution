9c09695306b1b208cabce5c384550dc650969fb4
“kebean” <chenqiua@gmail.com>
2024-06-09 17:05:14 +0200

    refactored code to be readable

--- challenges/stringTransform.js
+++ challenges/stringTransform.js
@@ -1,17 +1,16 @@
 function transformString(str) {
-    const length = str.length;
     let result = str;
 
     switch (true) {
-        case length % 15 === 0:
+        case str.length % 15 === 0:
             result = str.split('').reverse().join('');
-            result = result.split('').map(char => char.charCodeAt(0)).join('');
+            result = result.split('').map(char => char.charCodeAt(0)).join(' ');
             break;
-        case length % 3 === 0:
+        case str.length % 3 === 0:
             result = str.split('').reverse().join('');
             break;
-        case length % 5 === 0:
-            result = str.split('').map(char => char.charCodeAt(0)).join('');
+        case str.length % 5 === 0:
+            result = str.split('').map(char => char.charCodeAt(0)).join(' ');
             break;
         default:
             break;
